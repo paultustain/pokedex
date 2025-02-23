c5e9d0bf49d08a1e15a66981c689d76737d2388a
paultustain <p.tustain@hotmail.co.uk>
2025-02-23 06:09:04 +0000

    Updated help list

--- client_help.go
+++ client_help.go
@@ -7,6 +7,10 @@ func commandHelp(cfg *Config, name *string) error {
 Usage:
 
 help: Displays a help message
-exit: Exit the Pokedex`)
+exit: Exit the Pokedex
+catch: Attempts to catch a pokemon
+map: Returns a list of 20 locations at a time
+mapb: Goes back to the previous 20 lcoations
+Explore: Lists the available Pokemon in the area`)
 	return nil
 }
